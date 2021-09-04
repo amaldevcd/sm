ff98cf9bdae9024646aff2d28dd2cd060f242bb3
AMAL <mailtoamaldevcd@gmail.com>
2021-09-04 15:07:41 +0530

    talk to us

--- script.js
+++ script.js
@@ -17,3 +17,38 @@ $('#slider').owlCarousel({
         }
     }
 })
+$(document).ready(function(){
+    $(".back-to-top").hide();
+
+    $(window).scroll(function () {
+           if ($(this).scrollTop() > 1600) {
+               $('.back-to-top').fadeIn();
+           } else {
+               $('.back-to-top').fadeOut();
+           }
+       });
+       // scroll body to 0px on click
+       $('.back-to-top').click(function () {
+           $('.back-to-top').tooltip('hide');
+           $('body,html').animate({
+               scrollTop: 0
+           }, 800);
+           return false;
+       });
+       
+       $('.back-to-top').tooltip('show');
+
+});
+
+$(document).ready(function(){
+    $(".call").hide();
+
+    $(window).scroll(function () {
+           if ($(this).scrollTop() > 1600) {
+               $('.call').fadeIn();
+           } else {
+               $('.call').fadeOut();
+           }
+    });
+       $('.call').tooltip('show');
+});
